8bc1e562bcbd510ba812dbe8a608a0dd51b669ad
Oseyijotham <jothamoboh16@gmail.com>
2026-04-22 00:17:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="geo-pets">
+      <BrowserRouter basename="pawpoint">
         <App />
       </BrowserRouter>
     </Provider>
